7ca7f4a1c47ff76304aa0fda34417a6670e1d7a7
Tobiaszz1337 <tobias.zabloudil@educanet.cz>
2026-03-03 10:31:41 +0100

    Add files via upload

---
+++ Dokončení logiky registrace (práce se SESSION)/login.php
@@ -0,0 +1,34 @@
+<?php
+include_once("database.php");
+
+if (isset($_GET["logout"])) {
+    session_destroy();
+    header("Location: index.php");
+    exit();
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $stmt = $conn->prepare("SELECT * FROM users WHERE username = ?");
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows === 1) {
+        $user = $result->fetch_assoc();
+
+        if (password_verify($password, $user["password"])) {
+            $_SESSION["logged_user"] = $user["username"];
+            header("Location: index.php");
+            exit();
+        } else {
+            echo "Špatné heslo!";
+        }
+    } else {
+        echo "Uživatel neexistuje!";
+    }
+}
+?>
